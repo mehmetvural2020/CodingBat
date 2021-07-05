193ea4ef08a3fff5032cf42e8235dea002ee529a
mv <mehmetvurall@gmail.com>
2021-07-05 13:01:11 +0700

    The last two questions from warmup2

--- src/Warmup2/Has271.java
+++ src/Warmup2/Has271.java
@@ -17,19 +17,28 @@ public class Has271 {
         System.out.println(result);
     }
     public static boolean has271(int[] nums){
-        // Iterate < length-2, so can use i+1 and i+2 in the loop.
-        // Return true immediately when seeing 271.
-        for (int i=0; i < (nums.length-2); i++) {
-            int val = nums[i];
-            if (nums[i+1] == (val+5) &&              // the "7" check
-                    Math.abs(nums[i+2] - (val-1)) <= 2) {  // the "1" check
+        for(int i = 0; i< nums.length-2; i++){
+            if((nums[i+1]==nums[i]+5) && (Math.abs(nums[i+2]-(nums[i]-1))<=2))
                 return true;
-            }
         }
 
-        // If we get here ... none found.
         return false;
     }
 
+//public static boolean has271(int[] nums){
+//        // Iterate < length-2, so can use i+1 and i+2 in the loop.
+//        // Return true immediately when seeing 271.
+//        for (int i=0; i < (nums.length-2); i++) {
+//            int val = nums[i];
+//            if (nums[i+1] == (val+5) &&              // the "7" check
+//                    Math.abs(nums[i+2] - (val-1)) <= 2) {  // the "1" check
+//                return true;
+//            }
+//        }
+//
+//        // If we get here ... none found.
+//        return false;
+//    }
+//
 
 }
--- src/Warmup2/NoTriples.java
+++ src/Warmup2/NoTriples.java
@@ -14,11 +14,20 @@ public class NoTriples {
     }
 
     public static boolean noTriples(int[] nums) {
-        for (int i=0; i<nums.length-2;i++){
-            if((nums[i]==nums[i+1])&&(nums[i+1]==nums[i+2])) {
-                return false;
-            }
+        for (int i=0; i< nums.length-2; i++) {
+            if((nums[i] == nums[i+1]) && (nums[i]==nums[i+2]))return false;
         }
         return true;
     }
+    
+    
+    
+//    public static boolean noTriples(int[] nums) {
+//        for (int i=0; i<nums.length-2;i++){
+//            if((nums[i]==nums[i+1])&&(nums[i+1]==nums[i+2])) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 }
